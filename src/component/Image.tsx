f68d6167531829bf8ee2de13209ce10456efa5d4
Bandinopla <71508858+bandinopla@users.noreply.github.com>
2022-11-12 16:03:08 -0300

    limpiando un poco el diseño...

--- src/component/Image.tsx
+++ src/component/Image.tsx
@@ -37,6 +37,9 @@ export const Image: React.FC<{ src: string }> = ({ src }) => {
 
     return <>
         {loadedSrc != src ? <div style={{ height: 300, display: "flex", alignItems: "center", justifyContent: "center" }}>Cargando imagen...</div> : ""}
-        <a href={imageSrc} target="_blank"><img src={imageSrc} ref={ref} loading="eager" style={{ display: loadedSrc == src ? "block" : "none", margin: "0 auto", marginBottom: 20, minHeight: 300, maxWidth: 800 , border: "2px solid #ccc" }} /></a>
+        <a href={imageSrc} target="_blank" style={{display:"none"}}>
+            <img src={imageSrc} ref={ref} loading="eager" style={{ display: loadedSrc == src ? "block" : "none", margin: "0 auto", marginBottom: 20, minHeight: 300, maxWidth: 800 , border: "2px solid #ccc" }} />
+        </a>
+        { loadedSrc==src && <div onClick={()=>window.open(imageSrc,"_blank")} style={{ cursor:"pointer", maxWidth:"100%", marginBottom: 20, backgroundPositionY:"center", backgroundPositionX:"center", height:250, backgroundImage:`url(${imageSrc})`, backgroundRepeat:"no-repeat", backgroundSize:"contain"}}></div> }
     </>;
 }
--- src/component/Switch.tsx
+++ src/component/Switch.tsx
@@ -2,7 +2,7 @@
 
 export const Switch : React.FC<{ on:boolean, setTo:(newValue:boolean)=>void }> = ({ on, setTo }) =>{
 
-    return <div onClick={(ev)=>setTo(!on)} style={{ display:"inline-block", caret:"unset", cursor:"pointer", color:"white", fontWeight:"bold", backgroundColor:on?"green":"red", border:"2px solid #ccc", borderColor:on?"green":"red", padding:5, width:50}}>
+    return <div onClick={(ev)=>setTo(!on)} style={{ textAlign:"center", display:"inline-block", caret:"unset", cursor:"pointer", color:"white", fontWeight:"bold", backgroundColor:on?"green":"red", border:"2px solid #ccc", borderColor:on?"green":"red", padding:5, width:50}}>
                 {on?"Sí":"No"}
                 </div>
 }
