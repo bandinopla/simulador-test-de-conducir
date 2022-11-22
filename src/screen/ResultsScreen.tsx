7d5b118e41937ac984e287a1059fe04d74f1969a
Bandinopla <71508858+bandinopla@users.noreply.github.com>
2022-11-22 15:54:11 -0300

    alinear result gif... que se había desalineado...

--- src/screen/ResultsScreen.tsx
+++ src/screen/ResultsScreen.tsx
@@ -16,13 +16,13 @@ export const ResultsScreen: React.FC<{ quiz:Quiz, restart: () => void }> = ({ qu
             <Stat label="Puntaje de acierto">{percent}%</Stat>
         </div>
 
-        <div style={{marginTop:-20}} className="result-gifs-container">
-            { !aprobo && <iframe src="https://giphy.com/embed/tFMDtM35rm4V2" width="470" height="270" frameBorder="0" className="giphy-embed" allowFullScreen></iframe> }
-            { aprobo && percent<100 && <iframe src="https://giphy.com/embed/gVoBC0SuaHStq" width="270" height="280" frameBorder="0" className="giphy-embed" allowFullScreen></iframe> }
-            { aprobo && percent>=100 && <iframe src="https://giphy.com/embed/BPJmthQ3YRwD6QqcVD" width="480" height="270" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>}
+        <div style={{marginTop:-20}} className="result-gifs-container mb-9">
+            { !aprobo && <iframe src="https://giphy.com/embed/tFMDtM35rm4V2" width="470" height="270" frameBorder="0" className="giphy-embed drop-shadow-md" allowFullScreen></iframe> }
+            { aprobo && percent<100 && <iframe src="https://giphy.com/embed/gVoBC0SuaHStq" width="270" height="280" frameBorder="0" className="giphy-embed drop-shadow-md" allowFullScreen></iframe> }
+            { aprobo && percent>=100 && <iframe src="https://giphy.com/embed/BPJmthQ3YRwD6QqcVD" width="480" height="270" frameBorder="0" className="giphy-embed drop-shadow-md" allowFullScreen></iframe>}
         </div>
 
-        <div style={{padding:10, fontSize:"2em", margin:"40px 0", backgroundColor:aprobo?"green":"red", color:"white"}}>
+        <div style={{padding:10, fontSize:"2em",  backgroundColor:aprobo?"green":"red", color:"white"}} className="text-center">
             { aprobo?"Aprobaste!" : "Fallaste, seguí intentando."}
         </div>
 
